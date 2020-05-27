2edb724d9a10ba723c432077c2ea70e879f7663a
Johnatan Lima <johw.23@outlook.com>
2020-05-26 23:44:44 -0300

    Configurações de projeto

---
+++ Projeto-File/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Projeto_File.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
