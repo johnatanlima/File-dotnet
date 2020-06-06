608774d26e7e97a0ccdd1cda8048ca8cefabfb23
Johnatan Lima <johw.23@outlook.com>
2020-06-06 00:29:11 -0300

    Criação da classe de component

---
+++ Projeto-File/ViewComponents/UsuariosViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Projeto_File.Data;
+
+namespace AspNetCore
+{
+    public class UsuariosViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public UsuariosViewComponent(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            return View(await _appDbContext.Usuarios.ToListAsync());
+        }
+
+    }
+}
